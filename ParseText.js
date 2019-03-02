f0a988567941b976c7fbab96046c92808fa4e55b
Jason Guo <jasonyzguo@gmail.com>
2019-03-02 16:50:34 -0500

    add ParseText class

---
+++ ParseText.js
@@ -0,0 +1,31 @@
+/**
+ * This class extracts the important information from text
+ *
+ * @class
+ * @return the object that has the attributes and methods for this ParseText object
+ */
+ class ParseText {
+   /**
+    * Constructor function that creates a ParseText object
+    */
+   constructor() {
+     this.text = "";
+   }
+   /**
+    * Setter method that changes the text to be parsed
+    *
+    * @param {string} text - The text to be parsed
+    */
+    set text(text) {
+      this._text = text;
+    }
+
+   /**
+    * Getter method that returns the text that is to be parsed
+    *
+    * @returns {string} - The text that is to be parsed
+    */
+    get text() {
+      return this._text;
+    }
+ }
