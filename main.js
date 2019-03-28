c0b8795c793c7454e8dd84ee278805692f6b15a2
Jason Guo <jasonyzguo@gmail.com>
2019-03-28 10:14:39 -0400

    add polymorphism demonstration

--- main.js
+++ main.js
@@ -5,21 +5,15 @@ let ExtractTerms = require("./ExtractTerms");
   * Function used to test object functionality
   */
 function main() {
-  // let x = new StudyGuide();
-  // x.addTerm("mitosis");
-  //console.log(x.descriptions);
+  let parser = new ParseText();
+  let summarizer = new SummarizeText();
+  let extractor = new ExtractTerms();
 
-  let y = new ParseText();
-  y.text = "hello world";
-  //console.log(y.text);
-
-  let z = new SummarizeText();
-  z.text = `summarize text`;
-  z.callApi();
-
-  let a = new ExtractTerms();
-  a.text = `extract this`;
-  //console.log(a.keyTerms);
+  let textAPIs = [parser, summarizer, extractor];
+  textAPIs.forEach(api => {
+    api.text = "This API is working correctly";
+    if (typeof api.callApi === 'function') api.callApi();
+  });
 
 }
 main();
