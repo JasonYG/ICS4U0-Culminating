2359f930c310d3333874b028560d30742f91f415
Jason Guo <jasonyzguo@gmail.com>
2019-04-11 09:35:15 -0400

    add timing for sorting algorithms

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let ParseText = require("./ParseText");
 let SummarizeText = require("./SummarizeText");
 let ExtractTerms = require("./ExtractTerms");
+
+const fs = require('fs');
 /**
   * Function used to test object functionality
   */
@@ -13,12 +15,26 @@ async function main() {
   //let textAPIs = [summarizer, parser, extractor];
 
   //await summarizer.callApi();
-  //extractor.text = summarizer.summary;
-  extractor.text = `Allopatric speciation is typically subdivided into two major models: vicariance and peripatric. Both models differ from one another by virtue of their population sizes and geographic isolating mechanisms. The terms allopatry and vicariance are often used in biogeography to describe the relationship between organisms whose ranges do not significantly overlap but are immediately adjacent to each other—they do not occur together or only occur within in a narrow zone of contact. Historically, the language used to refer to modes of speciation directly reflected biogeographical distributions.[2] As such, allopatry is a geographical distribution opposed to sympatry (speciation within the same area). Furthermore, the terms allopatric, vicariant, and geographical speciation are often used interchangeably in the scientific literature.[2] This article will follow a similar theme, with the exception of special cases such as peripatric, centrifugal, among others.`;
-  await extractor.callApi();
 
+  console.time('Total time: ');
+  console.time('Read file: ');
+  extractor.terms = fs.readFileSync('manyTerms.txt', 'utf8').replace(/\r/g, "").split('\n');
+  console.timeEnd('Read file: ');
+  //await extractor.callApi();
+
+  console.time('Sorting: ');
   extractor.sortTerms();
-  console.log(extractor.keyTerms);
-  
+  console.timeEnd('Sorting: ');
+  //console.log(extractor.keyTerms);
+  console.timeEnd('Total time: ');
+
+  console.time('Write to file: ');
+  const stream = fs.createWriteStream('sortedTerms.txt');
+  stream.once('open', () => {
+    extractor.terms.map((currentTerm) => stream.write(term + "\n"));
+  });
+  console.timeEnd('Write to file: ');
+
+
 }
 main();
