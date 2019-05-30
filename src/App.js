447a0d108889b630695a31e4f7c55f07adc803c8
Jason Guo <jasonyzguo@gmail.com>
2019-05-30 10:25:13 -0400

    refactor main content into separate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import NavBar from "./components/navbar";
+import Home from "./components/home";
+
 import "./sass/App.scss";
 
 class App extends Component {
@@ -12,24 +15,20 @@ class App extends Component {
     return (
       <div className="App">
         <section className="hero  is-fullheight">
-          <div className="background" />
-          <div className="hero-head">
-            <NavBar
-              subheadings={this.state.subheadings}
-              imagePath={this.state.imagePath}
-            />
-          </div>
-          <div className="hero-body">
-            <div className="container has-text-centered">
-              <h1 className="title">Create a study guide today.</h1>
-              <h2 className="subtitle">
-                The Kool Kids Klub The Kool Kids Klub The Kool Kids Klub The
-                Kool Kids Klub The Kool Kids Klub The Kool Kids Klub The Kool
-                Kids Klub The Kool Kids Klub The Kool Kids Klub The Kool Kids
-                Klub The Kool Kids Klub The Kool Kids Klub The Kool Kids Klub
-              </h2>
+          <Router>
+            <div className="background" />
+            <div className="hero-head">
+              <NavBar
+                subheadings={this.state.subheadings}
+                imagePath={this.state.imagePath}
+              />
             </div>
-          </div>
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <Route path="/" component={Home} />
+              </div>
+            </div>
+          </Router>
         </section>
       </div>
     );
---
+++ src/components/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = props => {
+  return (
+    <React.Fragment>
+      <h1 className="title">Create a study guide today.</h1>
+      <h2 className="subtitle">
+        The Kool Kids Klub The Kool Kids Klub The Kool Kids Klub The Kool Kids
+        Klub The Kool Kids Klub The Kool Kids Klub The Kool Kids Klub The Kool
+        Kids Klub The Kool Kids Klub The Kool Kids Klub The Kool Kids Klub The
+        Kool Kids Klub The Kool Kids Klub
+      </h2>
+    </React.Fragment>
+  );
+};
+
+export default Home;
