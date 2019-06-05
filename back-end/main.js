057269f02f581fb0e2e56bb82fb98391015eb0d4
Jason Guo <jasonyzguo@gmail.com>
2019-06-05 12:38:17 -0400

    prepare deployment

--- back-end/main.js
+++ back-end/main.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../build/index.html"));
+});
 
 app.get("/api/test", (req, res) => res.send({ Hello: "world" }));
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 let ParseText = require("./ParseText");
