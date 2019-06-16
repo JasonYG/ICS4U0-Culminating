1210577318ec33bd4819a581402e65f035cf114d
Jason Guo <jasonyzguo@gmail.com>
2019-06-15 21:11:36 -0400

    add submit field

--- client/src/components/study.jsx
+++ client/src/components/study.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { throws } from "assert";
 
 class Study extends Component {
+  state = {
+    studyGuides: [],
+    topic: ""
+  };
   componentDidMount() {
     this.callApi();
   }
@@ -19,17 +24,61 @@ class Study extends Component {
     });
 
     const body = await test.json();
+    this.setState({ studyGuides: body.studyGuides ? body.studyGuides : [] });
     console.log(body);
   };
+  handleSubmit = async e => {
+    e.preventDefault();
+    console.log(this.state.topic);
+    const getTerm = await fetch("/api/search-term", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        term: this.state.topic
+      })
+    });
+    const body = await getTerm.json();
+    console.log(body);
+  };
+  handleChange = e => {
+    const state = { ...this.state };
+    state.topic = e.currentTarget.value;
+    this.setState({ topic: state.topic });
+  };
+
   render() {
+    const { studyGuides } = this.state;
     const isLoggedIn = localStorage.getItem("isLoggedIn");
     console.log("isLoggedIn ", isLoggedIn);
     if (isLoggedIn == "0") return <Redirect to="/" />;
 
     return (
       <React.Fragment>
-        <h1 className="title">Future of the study page.</h1>
-        <h2 className="subtitle">Placeholder Text</h2>
+        <h1 className="title">Welcome to Study4Life</h1>
+        {studyGuides.length !== 0 ? (
+          <h2 className="subtitle">{`You have ${
+            studyGuides.length
+          } study guides!`}</h2>
+        ) : (
+          <h2 className="subtitle">You have no study guides created!</h2>
+        )}
+        <form onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label htmlFor="" className="subtitle">
+              Enter a topic
+            </label>
+            <input
+              type="text"
+              placeholder="subject"
+              value={this.state.topic}
+              className="input"
+              onChange={this.handleChange}
+            />
+          </div>
+        </form>
       </React.Fragment>
     );
   }
