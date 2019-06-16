386ef47bd9c07e499d14eab1b9906dc840a14163
Jason Guo <jasonyzguo@gmail.com>
2019-06-15 21:10:26 -0400

    add async to login method

--- back-end/authentication.js
+++ back-end/authentication.js
@@ -19,26 +19,30 @@ class Authentication {
   /**
    * Logs user into service or registers a new user
    */
-  async login() {
+  login() {
     const email = this.userEmail;
     const MongoClient = require("mongodb").MongoClient;
     const uri =
       "mongodb+srv://studyguide:raaghavisgay@cluster0-pitl1.mongodb.net/test?retryWrites=true&w=majority";
-    MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
-      const db = client.db("studyguide");
-      const users = db.collection("users");
-      users.findOne({ email }, (err, user) => {
-        if (err) throw err;
-        if (user) {
-          users.updateOne(user, { $set: { updateOneIsWorking: "true" } });
-          return console.log(user);
-        }
-        users.insert({ email: email }, (err, inserted) => {
-          if (err) throw err;
-          if (inserted) console.log("Successfully added user!");
+    return new Promise((resolve, reject) =>
+      MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+        const db = client.db("studyguide");
+        const users = db.collection("users");
+        users.findOne({ email }, (err, user) => {
+          if (err) reject(err);
+          if (user) {
+            // users.updateOne(user, { $set: { studyGuides: [1, 2, 3, 4] } });
+            resolve(user);
+          } else {
+            const newUser = { email: email, studyGuides: [] };
+            users.insertOne(newUser, (err, inserted) => {
+              if (err) reject(err);
+              if (inserted) console.log("Successfully added user!");
+            });
+          }
         });
-      });
-    });
+      })
+    );
   }
 }
 module.exports = Authentication;
