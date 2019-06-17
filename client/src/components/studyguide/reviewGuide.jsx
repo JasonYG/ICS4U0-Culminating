93532eb66a582a3297ce0c691fd7546e601d8746
Jason Guo <jasonyzguo@gmail.com>
2019-06-17 00:15:39 -0400

    add direct to study guides

--- client/src/components/studyguide/reviewGuide.jsx
+++ client/src/components/studyguide/reviewGuide.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
 import Loading from "./loading";
+import StudyGuide from "./studyGuide";
+
 class ReviewGuide extends Component {
-  state = {};
+  state = {
+    finishLoading: true,
+    studyGuide: {
+      topic: "Animal",
+      content: [
+        {
+          term: "Ethymology",
+          info: "Information about it",
+          subtopics: [
+            {
+              term: "1",
+              info: "Information about it",
+              subtopics: [
+                { term: "3", info: "Information about it", subtopics: [] },
+                { term: "4", info: "Information about it", subtopics: [] }
+              ]
+            },
+            { term: "2", info: "Information about it", subtopics: [] }
+          ]
+        },
+        { term: "Mammals", info: "Information about it", subtopics: [] }
+      ]
+    }
+  };
   componentDidMount() {
-    this.callApi();
+    if (this.props.currentGuide != null) this.callApi();
   }
   callApi = async () => {
     const {
@@ -26,11 +51,50 @@ class ReviewGuide extends Component {
       })
     });
     const body = await getGuide.json();
-    console.log(body);
-    this.setState({ finishLoading: true });
+
+    this.setState({ studyGuide: body, finishLoading: true });
+  };
+  handleDelete = () => {
+    this.props.history.replace("/study/");
+  };
+  handleBack = () => {
+    this.props.history.replace("/study/");
+  };
+  handleSave = async () => {
+    const saveGuide = await fetch("/api/save-guide", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        idToken: localStorage.getItem("idToken"),
+        studyGuide: this.state.studyGuide
+      })
+    });
+    const body = await saveGuide.json();
+
+    this.props.history.replace("/study/");
   };
   render() {
-    if (this.state.finishLoading != null) console.log(this.props);
+    if (this.props.currentGuide != null)
+      return (
+        <StudyGuide
+          onDelete={this.handleDelete}
+          //TODO: Implement delete
+          onBack={this.handleBack}
+          guide={this.props.currentGuide}
+        />
+      );
+    if (this.state.finishLoading != null) {
+      return (
+        <StudyGuide
+          onDelete={this.handleDelete}
+          onSave={this.handleSave}
+          guide={this.state.studyGuide}
+        />
+      );
+    }
     return (
       <React.Fragment>
         <Loading />
---
+++ client/src/components/studyguide/studyGuide.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+class StudyGuide extends Component {
+  parseStudyGuide = () => {
+    const { guide } = this.props;
+    return guide.content.map(section => {
+      const recursivelyObtainTopics = section => {
+        if (section == null || section.length === 0) {
+          return;
+        }
+        const { term, info, subtopics } = section;
+        return (
+          <React.Fragment>
+            <h2 className="subtitle form-label has-text-centered is-3">
+              {term}
+            </h2>
+            <p className="subtitle form-label">{info}</p>
+            {subtopics.map(topic => recursivelyObtainTopics(topic))}
+          </React.Fragment>
+        );
+      };
+      return recursivelyObtainTopics(section);
+    });
+  };
+
+  render() {
+    const { guide } = this.props;
+    return (
+      <React.Fragment>
+        <h1 className="title has-text-centered">{guide.topic}</h1>
+        {this.parseStudyGuide()}
+        <div className="columns">
+          <div className="column is-half">
+            {this.props.onBack == null ? (
+              <a
+                className="button is-primary is-inverted is-outlined subtitle is-4 is-fullwidth"
+                onClick={this.props.onSave}
+              >
+                Save
+              </a>
+            ) : (
+              <a
+                className="button is-primary is-inverted is-outlined subtitle is-4 is-fullwidth"
+                onClick={this.props.onBack}
+              >
+                Back
+              </a>
+            )}
+          </div>
+          <div className="column is-half">
+            <a
+              className="button is-primary is-inverted is-outlined subtitle is-4 is-fullwidth"
+              onClick={this.props.onDelete}
+            >
+              Delete
+            </a>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default StudyGuide;
